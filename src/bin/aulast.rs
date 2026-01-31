fe5efda3266955b8e814bf48a6771efb77d5b246
Callie Foster <brody.foster@gmail.com>
2026-01-30 20:31:27 -0500

    Added scaffolding for CLI tools (ausearch, aulast)

---
+++ src/bin/aulast.rs
@@ -0,0 +1,4 @@
+// To run this, use `cargo run --bin aulast`
+fn main() {
+    println!("Aulast binary - placeholder");
+}
---
+++ src/bin/ausearch.rs
@@ -0,0 +1,4 @@
+// To run this, use `cargo run --bin ausearch`
+fn main() {
+    println!("Ausearch binary - placeholder");
+}
